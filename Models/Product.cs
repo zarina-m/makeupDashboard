eb4cfdab42adc1bf3ebea10d2f14b01a0b13b57c
Josh Varty <joshvarty@gmail.com>
2020-08-08 20:42:29 -0400

    Hook app up to database

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace makeupDashboard.Models
+{
+    public partial class Product
+    {
+        public Guid Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Retailer { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public string Size { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public decimal? NewPrice { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public DateTime? SaleDate { get; set; }
+    }
+}
---
+++ Models/makeupDBContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+namespace makeupDashboard.Models
+{
+    public partial class makeupDBContext : DbContext
+    {
+        static string connectionString {get;set;} = Environment.GetEnvironmentVariable("MAKEUP", EnvironmentVariableTarget.Process);
+        public makeupDBContext()
+        {
+        }
+
+        public makeupDBContext(DbContextOptions<makeupDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.AddedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Brand)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Colour)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ImageUrl)
+                    .IsRequired()
+                    .HasColumnName("ImageURL")
+                    .HasMaxLength(2083)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NewPrice).HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.OriginalPrice).HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("Name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Retailer)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.SaleDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Size)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
